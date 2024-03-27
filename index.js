0ce80e913a30e533078488f121f88b965b91487b
Mikhail <michaildrazdovich@gmail.com>
2024-03-27 10:39:40 +0300

    algorhytms

--- index.js
+++ index.js
@@ -3,13 +3,14 @@
 function reverse(array) {
   const copyArr = JSON.parse(JSON.stringify(array));
   let buf;
-  let length = copyArr.length;
-  for (let i = 0; i < length / 2; i++) {
-    let index = length - 1 - i;
+
+  for (let i = 0; i < copyArr.length / 2; i++) {
+    const endIndex = array.length - 1 - i;
     buf = copyArr[i];
-    copyArr[i] = copyArr[index];
-    copyArr[index] = buf;
+    copyArr[i] = copyArr[endIndex];
+    copyArr[endIndex] = buf;
   }
+
   return copyArr;
 }
 
@@ -82,6 +83,19 @@ function getData() {
 // Если вызывалась недавно, то вернуть сохраненный результат;
 // Если вызывалась давно, заново сходить за данными.
 
+// function cache(getData, ts) {
+//   let lastCall;
+//   let cache;
+//
+//   return async () => {
+//     if (!lastCall || Date.now() - lastCall >= ts) {
+//       cache = await getData();
+//       lastCall = Date.now();
+//     }
+//
+//     return cache;
+//   };
+// }
 
 function cache(getData, ts) {
   let lastCall;
@@ -97,13 +111,30 @@ function cache(getData, ts) {
   };
 }
 
+function throttle(fn, timeout) {
+  let inThrottle = false;
+
+  return (...args) => {
+    const context = this;
+
+    if (!inThrottle) {
+      fn.apply(context, args);
+      inThrottle = true;
+
+      setTimeout(() => {
+        inThrottle = false;
+      }, timeout);
+    }
+  };
+}
+
 function debounce(fn, timeout) {
   let timer;
 
   return (...args) => {
     clearTimeout(timer);
 
-    timer = setTimeout(() => {
+    setTimeout(() => {
       fn.apply(this, args);
     }, timeout);
   };
@@ -129,10 +160,10 @@ function isPalindrome(str) {
   let end = 0;
 
   while (start < end) {
-    const startChar = str[start];
+    const firstChar = str[start];
     const endChar = str[end];
 
-    if (!isLetter(startChar)) {
+    if (!isLetter(firstChar)) {
       start += 1;
       continue;
     }
@@ -142,7 +173,7 @@ function isPalindrome(str) {
       continue;
     }
 
-    if (!isEqual(start, endChar)) {
+    if (!isEqual(firstChar, endChar)) {
       return false;
     }
 
@@ -155,8 +186,9 @@ function isPalindrome(str) {
 }
 
 function testFn(array) {
-  const buf = array.filter(value => value % 2 === 1).sort((a, b) => a - b);
+  const buf = array.filter((value) => value % 2 === 1).sort((a, b) => a - b);
   let bufIndex = 0;
+
   for (let i = 0; i < array.length; i++) {
     if (array[i] % 2 === 1) {
       array[i] = buf[bufIndex];
@@ -167,8 +199,6 @@ function testFn(array) {
   return array;
 }
 
-
-// reduceRight
 function compose(...functionArray) {
   return function(...args) {
     return functionArray.reduceRight((acc, val) => {
@@ -179,6 +209,7 @@ function compose(...functionArray) {
 
 // var func2 = bind(func11, this, args);
 // func2();
+
 function bind(func, ctx, ...args) {
   return function(...newArgs) {
     return func.apply(ctx, [...args, ...newArgs]);
@@ -219,7 +250,6 @@ function flatten(list) {
     }
   }
 
-
   return result;
 }
 
@@ -235,32 +265,325 @@ function flatten(list) {
 
 // limits = [[1000, 6], [100, 5], [50, 1]]
 
+
 function atm(sum, limit) {
-  const sortedLimits = limit.sort((a, b) => b[0] - a[0]);
-  const smallestBanknote = sortedLimits[sortedLimits.length - 1][0];
+  const sortedLimit = limit.sort((a, b) => b[0] - a[0]);
+  const smallestBanknote = sortedLimit[sortedLimit.length - 1][0];
   const result = [];
 
-  if(sum % smallestBanknote !== 0) {
+  if (sum % smallestBanknote !== 0) {
     return 'Error: Incorrect value';
   }
 
-  sortedLimits.forEach((limit) => {
-    const [nominal, limitAmount] = limit;
-    const currentBanknoteAmount = Math.floor(sum / nominal);
-    const currentAmount = currentBanknoteAmount > limitAmount ? limitAmount : currentBanknoteAmount;
-    sum -= currentAmount * nominal;
-    result.push([currentAmount, nominal]);
+  sortedLimit.forEach((limit) => {
+    const [banknote, amount] = limit;
+    const max = Math.floor(sum / banknote);
+    const currentAmount = max > amount ? amount : max;
+    sum -= currentAmount * banknote;
+    result.push([currentAmount, banknote]);
   });
 
-  if(sum === 0) {
-    result.forEach((value, index) => {
-      sortedLimits[index][1] -= value[0]
+  if (sum === 0) {
+    result.forEach((res) => {
+      sortedLimit[1] -= res[0];
     });
 
-    return result.filter((x) => x[0]).map((x) => x.join('x')).join(' ');
+    return result.filter((val) => val[0]).map((val) => val.join('x')).join(' ');
   } else {
     return 'Error: Not enough money';
   }
+}
+
+/**
+ * Дан массив точек с целочисленными координатами (x, y).
+ * Определить, существует ли вертикальная прямая,
+ * делящая точки на 2 симметричных относительно этой прямой множества.
+ * Note: Для удобства точку можно представлять не как массив [x, y], а как объект {x, y}
+ */
+
+// [
+//   [0, 0],
+//   [10, 0],
+// ]
+
+function isVertSym(list) {
+  const cache = {};
+  let minX = Math.min();
+  let maxX = Math.max();
+  list.forEach(point => {
+    minX = Math.min(minX, point[0]);
+    maxX = Math.max(maxX, point[0]);
+    const key = point.join(',');
+    if (cache[key] === undefined) {
+      cache[key] = 0;
+    }
+    cache[key]++;
+  });
+  const centerX = (minX + maxX) / 2;
+  return list.every(point => {
+    let simX = point[0] > centerX ? centerX - (point[0] - centerX) : centerX + (centerX - point[0]);
+    simX = minX - point[0] + maxX;
+    let simY = point[1];
+    const key = [simX, simY].join(',');
+    if (cache[key]) {
+      cache[key]--;
+      return true;
+    } else {
+      return false;
+    }
+  });
+}
+
+//Дан список слов.
+//Каждое слово представлено массивом возможных вариантов нормализации.
+//Написать функцию, которая принимает список слов и возвращает функцию,
+//которая при каждом вызове возвращает строку -
+//одно из возможных сочетаний вариантов слова в предложении.
+//Пока не вернёт все возможные варианты.
+//Нужна реализация без итераторов.
+//Ни список слов, ни варианты нормализации не будут пустыми.
+//Внучку –> ['внучка', 'внучок', ...]
+//Машу –> ['маша', 'махать', 'машу', ...]
+//...
+
+// ['внучка', 'внучок'],
+//   ['маша', 'махать', 'машу']
+function allSentences(aoa) {
+  const stack = aoa.map(x => 0);
+
+  return () => {
+    if (stack.length === 0 || stack[0] === aoa[0].length) {
+      return;
+    }
+    const combination = stack.map((elem, index) => (aoa[index][elem])).join(' ');
+    let aoaIndex = stack.length - 1;
+    while (stack[aoaIndex] === aoa[aoaIndex].length) {
+      stack[aoaIndex] = 0;
+      stack[aoaIndex - 1]++;
+      aoaIndex--;
+    }
+    return combination;
+  };
+}
+
+/*
+У нас есть набор билетов вида:
+
+[
+ { from: 'London', to: 'Moscow' },
+ { from: 'NY', to: 'London' },
+ { from: 'Moscow', to: 'SPb' },
+ ...
+]
+
+Из этих билетов можно построить единственный, неразрывный маршрут.
+Петель и повторов в маршруте нет.
+
+Нужно написать программу, которая возвращает билеты
+в порядке следования по маршруту.
+*/
+
+// const tickets = [
+//   { from: 'London', to: 'Moscow' },
+//   { from: 'NY', to: 'London' },
+//   { from: 'Moscow', to: 'SPb' },
+// ]
+
+// const fromCache = {
+//   "London": {
+//     "from": "London",
+//     "to": "Moscow"
+//   },
+//   "NY": {
+//     "from": "NY",
+//     "to": "London"
+//   },
+//   "Moscow": {
+//     "from": "Moscow",
+//     "to": "SPb"
+//   }
+// }
+
+// const toCache = {
+//   "Moscow": {
+//     "from": "London",
+//     "to": "Moscow"
+//   },
+//   "London": {
+//     "from": "NY",
+//     "to": "London"
+//   },
+//   "SPb": {
+//     "from": "Moscow",
+//     "to": "SPb"
+//   }
+// }
+
+function getRoute(tickets = []) {
+  if (!tickets.length) {
+    return [];
+  }
+
+  const fromCache = {};
+  const toCache = {};
+  const way = [];
+
+  tickets.forEach((ticket) => {
+    fromCache[ticket.from] = ticket;
+    toCache[ticket.to] = ticket;
+  });
+
+  let currentPont = tickets[0];
+
+  while (toCache[currentPont.from]) {
+    currentPont = toCache[currentPont.from];
+  }
+
+  way.push(currentPont);
+
+  while (fromCache[currentPont.to]) {
+    currentPont = fromCache[currentPont.to];
+    way.push(currentPont);
+  }
+
+  return way;
+}
+
+// функция поиска вариантов перелетов из точки
+
+// Например, для
+
+// findPath('A', 'N', fetchFlights).then((result) => console.log(result)); // Promise.resolve(['A', 'B', 'N']);)
+// findPath('A', 'S', fetchFlights).then((result) => console.log(result)); // Promise.resolve(['A', 'D', 'F', 'S']);)
+// findPath('B', 'S', fetchFlights).then((result) => console.log(result)); // Promise.reject(new Error('No way');)
+
+// graph = { A: [B, D], B: [C, N, Z], D: [E, F], F: [S] }
+
+async function findPath(start, end, fetchFlights) {
+  const visited = new Set();
+  const stack = [
+    [start, [start]],
+  ];
+
+  while (stack.length) {
+    const [currentNode, path] = stack.pop();
+
+    if (currentNode === end) {
+      return path;
+    }
+
+    if (!visited.has(currentNode)) {
+      visited.add(currentNode);
+
+      const neighbours = await fetchFlights(currentNode) || [];
+      for (let neighbour of neighbours) {
+        if (!visited.has(neighbour)) {
+          stack.push([neighbour, [...path, neighbour]]);
+        }
+      }
+    }
+  }
+
+  return 'No way';
+}
+
+// Дан упорядоченный массив натуральных чисел,
+// повторяющихся элементов в списке нет.
+// Нужно преобразовать в строку с перечислением интервалов через запятую.
+
+// Пример:
+// [2, 3, 5, 6, 7, 8, 9, 11, 20, 21, 22] -> “2-3,5-9,11,20-22”
+
+function convertToIntervalString(arr) {
+  let result = [];
+  let start = arr[0];
+  let end = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] === end + 1) {
+      end = arr[i];
+    } else {
+      result.push(start === end ? start.toString() : `${start}-${end}`);
+      start = arr[i];
+      end = arr[i];
+    }
+  }
+
+  result.push(start === end ? start.toString() : `${start}-${end}`);
+
+  return result.join(',');
+}
+
+// Нужно реализовать структуру данных, которая реализует методы:
+// а) like(userId) – пользователь userId поставил лайк
+// б) unlike(userId) – пользователь userId убрал лайк
+// в) getMax() – узнать максимальное число лайков, которое поставил один пользователь
+
+class LikesCounter {
+  constructor() {
+    this.likes = {};
+  }
+
+  like(userId) {
+    if (this.likes[userId] === undefined) {
+      this.likes[userId] = 1;
+    } else {
+      this.likes[userId] += 1;
+    }
+  }
+
+  unlike(userId) {
+    if (this.likes[userId]) {
+      this.likes[userId] -= 1;
+
+      if (this.likes[userId] < 0) {
+        this.likes = 0;
+      }
+    }
+  }
+
+  getMax() {
+    const values = Object.values(this.likes);
+    return Math.max(...Object.values(this.likes));
+  }
+}
+
+// На улице стоят n непокрашенных домов в ряд.
+// Известна стоимость покраски каждого дома в один из k цветов.
+// Жильцы этих домов большие завистники, поэтому никакие два дома, стоящие рядом, не могут иметь одинакового покраса.
+// Найти минимальную стоимость покраски всех домов при указанных условиях.
+// Входные данные: houses[][k] – массив стоимости покраски в каждый цвет для каждого дома.
+
+
+// [
+//    [1, 2, 3],
+//    [1, 3, 2],
+//    [3, 1, 2]
+// ] → 4
+
+function minCostPainting(houses) {
+  const n = houses.length;
+  const k = houses[0].length;
+  const dp = Array.from({ length: n }, () => Array(k).fill(0));
+
+  // Инициализация первого дома
+  for (let j = 0; j < k; j++) {
+    dp[0][j] = houses[0][j];
+  }
+
+  for (let i = 1; i < n; i++) {
+    for (let j = 0; j < k; j++) {
+      let minCost = Infinity;
+      for (let prevColor = 0; prevColor < k; prevColor++) {
+        if (prevColor !== j) {
+          minCost = Math.min(minCost, dp[i - 1][prevColor]);
+        }
+      }
+      dp[i][j] = houses[i][j] + minCost;
+    }
+  }
+
+  return Math.min(...dp[n - 1]);
 }
 
 module.exports = {
@@ -274,4 +597,11 @@ module.exports = {
   compose,
   flatten,
   atm,
+  isVertSym,
+  allSentences,
+  getRoute,
+  findPath,
+  convertToIntervalString,
+  LikesCounter,
+  minCostPainting
 };
--- index.test.js
+++ index.test.js
@@ -8,7 +8,14 @@ const {
   testFn,
   compose,
   flatten,
-  atm
+  atm,
+  isVertSym,
+  allSentences,
+  getRoute,
+  findPath,
+  convertToIntervalString,
+  LikesCounter,
+  minCostPainting
 } = require('./index');
 
 describe('reverse', () => {
@@ -64,7 +71,7 @@ describe('getRandomHexColor', () => {
 });
 
 describe('cache', () => {
-  it('test cache', () => {
+  it('test cache', async () => {
     function getData() {
       return new Promise(resolve => {
         setTimeout(() => {
@@ -75,7 +82,8 @@ describe('cache', () => {
     }
 
     const newGet = cache(getData, 5000);
-    newGet();
+    const result1 = await newGet();
+    const result2 = await newGet();
   });
 });
 
@@ -124,11 +132,13 @@ describe('compose', () => {
 
 describe('flatten', () => {
   it('test case 1', () => {
-    const input =   JSON.parse(JSON.stringify([1, 'any [complex] string', null, function() {}, [1, 2, [3, '4'], 0], [], { a: 1 }]));
-    const output =  JSON.parse(JSON.stringify([1, 'any [complex] string', null, function() {}, 1, 2, 3, '4', 0, { a: 1 }]));
+    const input = JSON.parse(JSON.stringify([1, 'any [complex] string', null, function() {
+    }, [1, 2, [3, '4'], 0], [], { a: 1 }]));
+    const output = JSON.parse(JSON.stringify([1, 'any [complex] string', null, function() {
+    }, 1, 2, 3, '4', 0, { a: 1 }]));
 
     expect(flatten(input)).toEqual(output);
-  })
+  });
 });
 
 describe('atm', () => {
@@ -153,6 +163,163 @@ describe('atm', () => {
   it('test case 4', () => {
     const limits = [[1000, 6], [100, 5], [50, 1]]; // нужно выбрать любой удобный формат
 
-    expect(atm(6512, limits)).toEqual('Error: Incorrect value') // 'Error: Incorrect value'
+    expect(atm(6512, limits)).toEqual('Error: Incorrect value'); // 'Error: Incorrect value'
   });
+});
+
+describe('isVertSym', () => {
+  it('test case 1', () => {
+    const input = [
+      [0, 0],
+      [0, 0],
+      [1, 1],
+      [2, 2],
+      [3, 1],
+      [4, 0],
+      [4, 0],
+    ];
+    const result = isVertSym(input); // true
+    expect(result).toBeTruthy();
+  });
+
+  it('test case 2', () => {
+    const input = [
+      [0, 0],
+      [0, 0],
+      [1, 1],
+      [2, 2],
+      [3, 1],
+      [4, 0],
+    ];
+    const result = isVertSym(input); // false
+    expect(result).toBeFalsy();
+  });
+
+  it('test case 3', () => {
+    const input = [];
+    const result = isVertSym(input); // true
+    expect(result).toBeTruthy();
+  });
+
+  it('test case 4', () => {
+    const input = [
+      [0, 0],
+    ];
+    const result = isVertSym(input); // true
+    expect(result).toBeTruthy();
+  });
+
+  it('test case 5', () => {
+    const input = [
+      [0, 0],
+      [10, 0],
+    ];
+
+    const result = isVertSym(input); // true
+    expect(result).toBeTruthy();
+  });
+
+  it('test case 6', () => {
+    const input = [
+      [0, 0],
+      [11, 1],
+    ];
+    const result = isVertSym(input); // false
+    expect(result).toBeFalsy();
+  });
+
+  it('test case 1', () => {
+    const input = [
+      [0, 0],
+      [1, 0],
+      [3, 0],
+    ];
+    const result = isVertSym(input); // false
+    expect(result).toBeFalsy();
+  });
+});
+
+describe('allSentences', () => {
+  it('test case 1', () => {
+    const nextSentence = allSentences([
+      ['внучка', 'внучок'],
+      ['маша', 'махать', 'машу'],
+      // ... тут могут быть другие слова с любым количеством своих форм
+    ]);
+    expect(nextSentence()).toEqual('внучка маша'); // 'внучка маша'
+    expect(nextSentence()).toEqual('внучка махать'); // 'внучка махать'
+    expect(nextSentence()).toBeUndefined(); // undefined
+  });
+});
+
+describe('getRoute', () => {
+  it('test case 1', () => {
+    const tickets = [
+      { from: 'London', to: 'Moscow' },
+      { from: 'NY', to: 'London' },
+      { from: 'Moscow', to: 'SPb' },
+    ];
+    const result = getRoute(tickets);
+    expect(result).toEqual([
+      { from: 'NY', to: 'London' },
+      { from: 'London', to: 'Moscow' },
+      { from: 'Moscow', to: 'SPb' },
+    ]);
+  });
+});
+
+describe('findPath', () => {
+  const graph = { A: ['B', 'D'], B: ['C', 'N', 'Z'], D: ['E', 'F'], F: ['S'] };
+
+  async function fetchFlights(from) {
+    return graph[from];
+  }
+
+  it('case 1', async () => {
+    const result = await findPath('A', 'N', fetchFlights); // Promise.resolve(['A', 'B', 'N']);)
+    expect(result).toEqual(['A', 'B', 'N']);
+  });
+
+  it('case 2', async () => {
+    const result = await findPath('A', 'S', fetchFlights); // Promise.resolve(['A', 'D', 'F', 'S']);)
+    expect(result).toEqual(['A', 'D', 'F', 'S']);
+  });
+
+  it('case 3', async () => {
+    const result = await findPath('B', 'S', fetchFlights); // Promise.reject(new Error('No way');)
+    expect(result).toEqual('No way');
+  });
+});
+
+describe('convertToIntervalString', () => {
+  it('test case 1', () => {
+    const input = [2, 3, 5, 6, 7, 8, 9, 11, 20, 21, 22];
+    expect(convertToIntervalString(input)).toEqual('2-3,5-9,11,20-22');
+  });
+});
+
+describe('LikesCounter', () => {
+  it('test case 1', () => {
+    const likesCounter = new LikesCounter();
+    likesCounter.like(1);
+    likesCounter.like(2);
+    likesCounter.like(1);
+    likesCounter.like(3);
+    likesCounter.like(2);
+    likesCounter.like(2);
+
+    expect(likesCounter.getMax()).toEqual(3); // Выведет: 3
+  });
+});
+
+describe('minCostPainting', () => {
+  it('test case 1', () => {
+    // Пример использования
+    const houses = [
+      [1, 2, 3],
+      [1, 3, 2],
+      [3, 1, 2]
+    ];
+    expect(minCostPainting(houses)).toEqual(4); // Выведет: 4
+  })
 })
